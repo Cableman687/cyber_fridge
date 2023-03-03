c2ee241998bb451d028f4fef89f7db6804ed67f7
HelenELee <HelenELee3@outlook.com>
2023-03-03 10:34:42 +0800

    added Model files for Recipe and Ingredient

--- models/index.js
+++ models/index.js
@@ -1,3 +1,23 @@
 const User = require('./User');
+const Ingredient = require('./Ingredient');
+const Recipe = require('./Recipe');
 
-module.exports = { User};
+User.hasMany(Ingredient, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Ingredient.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+User.hasMany(Recipe, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Recipe.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+module.exports = { User, Ingredient, Recipe};
