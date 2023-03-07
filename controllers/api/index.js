bfa6549dbd312996cf8ac0a416dcb0587415afca
William <will_cable@hotmail.com>
2023-03-07 15:38:56 +0800

    Addition of Title to login page, and Account Creation Funcionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const ingredientRoutes = require('./ingredientRoutes');
 
 router.use('/users', userRoutes);
+router.use('/ingredients', ingredientRoutes);
 
 module.exports = router;
