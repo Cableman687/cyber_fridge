44da9f1b2eded26e67f71f1ecd37e26c3feb5574
William <will_cable@hotmail.com>
2023-03-12 15:30:02 +0800

    Addition of API call to generate random food fact, and homepage refactoring

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
 const hbs = exphbs.create({ helpers });
 
 // Configure and link a session object with the sequelize store
